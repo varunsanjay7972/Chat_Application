dfe0340ea497f81def2cbc4355a2984bfa7a336f
varunsanju7972 <sanjay.varun@iitg.ac.in>
2024-08-24 01:56:33 +0530

    removed TSL certificate error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import https from 'https';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
         proxy: {
-            "/api": {
-                target: "https://chat-application-backend-63yg.onrender.com/",
+            // "/api": {
+            //     target: "https://chat-application-backend-63yg.onrender.com/",
+            // },
+            '/api': {
+                target: 'https://chat-application-backend-63yg.onrender.com/',
+                changeOrigin: true,
+                secure: false, // This disables SSL verification (not recommended for production)
+                agent: new https.Agent({
+                    rejectUnauthorized: false, // This allows self-signed certificates
+                }),
             },
         },
     },
